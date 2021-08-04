5828df58b70e7e673a625914cefed1f42456661d
Anthony Ahumada <locopala99@gmail.com>
2021-08-04 09:55:07 +0200

    Mean

--- Mean/Project/ScrumBoard/backend/db/db.js
+++ Mean/Project/ScrumBoard/backend/db/db.js
@@ -1,1 +1,11 @@
 const mongoose = require("mongoose");
+
+//creamos una funcion flecha que nos ayuda a conectar a la bd.
+
+const dbConnection = async () =>{
+    try {
+        
+    } catch (error) {
+        
+    }   
+}
