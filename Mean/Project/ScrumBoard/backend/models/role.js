476ac762eadeac25e08bad666dd9eb3ceeca84f1
Anthony Ahumada <locopala99@gmail.com>
2021-08-06 12:00:16 +0200

    more mongo

--- Mean/Project/ScrumBoard/backend/models/role.js
+++ Mean/Project/ScrumBoard/backend/models/role.js
@@ -1,3 +1,12 @@
 //importamos la libreria el modulo de moongose , esto nos ayuda con todo lo de mong
 //nos ayuda a crear el esquema de la base de datos
 const mongoose = require('mongoose');
+
+//vamos a crear el esquema o diagrama del rol.
+//moongose tiene una libreria interna que se llama esquema
+//queremos que se guarde un JSON 
+const roleSchema = new mongoose.Schema({
+    name:string,
+    description:string,
+    date:date,
+})
