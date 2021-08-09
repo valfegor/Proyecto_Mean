d7e8518e0ab2039df89ffdc67a327e1c5adbe1f9
Anthony Ahumada <locopala99@gmail.com>
2021-08-09 08:33:18 +0200

    board

--- Mean/Project/ScrumBoard/backend/models/board.js
+++ Mean/Project/ScrumBoard/backend/models/board.js
@@ -1,3 +1,18 @@
 //Se van a guardar los datos del usuario.
 
 const mongoose = require("mongoose");
+
+//construccion de la bd board
+const boardSchema = new mongoose.Schema({
+    userId:{type:mongoose.schema.ObjectId,ref:"user"},
+    name:String,
+    description:String,
+    taskStatus:String,
+    imageUrl:String,
+    date: { type: Date, default: Date.now },
+});
+
+
+const board = mongoose.model("board",boardSchema);
+
+module.exports = board;
