d880dce3f2c1660b85b4c2ca0bd91fd26f62e07b
Anthony Ahumada <locopala99@gmail.com>
2021-08-09 08:28:14 +0200

    more board

---
+++ Mean/Project/ScrumBoard/backend/models/board.js
@@ -0,0 +1,3 @@
+//Se van a guardar los datos del usuario.
+
+const mongoose = require("mongoose");
--- Mean/Project/ScrumBoard/backend/models/user.js
+++ Mean/Project/ScrumBoard/backend/models/user.js
@@ -2,14 +2,23 @@
 const mongoose = require("mongoose");
 
 //invoco la construccion del schema
+//el esquema ya fue creado
 const userSchema = new mongoose.Schema({
 name:String,
 email:String,
 password:String,
 //nos traemos el mongoose que es la variable , mongoose maneja esquemas
-//apunta a la coleccion rol
+//apunta a la coleccion rol , es un tipo de datos que retorna un string
 roleId:{type:mongoose.Schema.ObjectId,ref:"role"},
 date:{type:Date, default:Date.now},
 dbStatus:Boolean,
 
 });
+
+//ahora debemos exportarlo.
+//va a ir todo lo que llegue a mondo y en la coleccion user va a guardar todos los datos en mongo BD
+const user = mongoose.model("user",userSchema);
+
+//siempre va el module.
+
+module.exports=user;
