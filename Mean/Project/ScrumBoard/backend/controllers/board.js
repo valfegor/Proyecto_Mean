e2153d804c5fc7438e3efcc52b81d4f19e859102
Anthony Ahumada <locopala99@gmail.com>
2021-08-17 08:58:35 +0200

    -more jd

--- Mean/Project/ScrumBoard/backend/controllers/board.js
+++ Mean/Project/ScrumBoard/backend/controllers/board.js
@@ -4,6 +4,18 @@ const saveTask = async (req,res) => {
 
     if(!req.body.name || !req.body.description) return res.status(400).send("Incomplete data");
 
+
+    const board = new Board({
+        userId:req.body.userId,
+        name:req.body.name,
+        description:req.body.description,
+        taskStatus:"to-do",
+    });
+
+    const result = await board.save();
+
+    if(!result) return res.status(400).send("Error cant upload the task");
+
 }
 
 module.exports = {saveTask};
