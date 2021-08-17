21d62a6f15580ed654a7005cf6a590d25ab248f1
Anthony Ahumada <locopala99@gmail.com>
2021-08-17 08:45:49 +0200

    More js

--- Mean/Project/ScrumBoard/backend/middleware/auth.js
+++ Mean/Project/ScrumBoard/backend/middleware/auth.js
@@ -6,5 +6,11 @@ const jwt = require('jsonwebtoken');
 
 
 const auth = async (req, res, next) => {
-    
+    //estoy en la parte de autorization , es decir se va al authorization del header.
+    let jwtToken = req.header("Authorization");
+
+    //si no hay token.
+
+    if(!jwtToken) return res.status(400).send("Authorization denied : No token");
+
 }
