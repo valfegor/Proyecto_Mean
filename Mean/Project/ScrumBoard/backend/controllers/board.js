73252c257644735a693ab9835399b2b1c5f83927
Anthony Ahumada <locopala99@gmail.com>
2021-08-17 08:54:51 +0200

    More js

--- Mean/Project/ScrumBoard/backend/controllers/board.js
+++ Mean/Project/ScrumBoard/backend/controllers/board.js
@@ -2,6 +2,7 @@ const Board  = require('../models/board');
 
 const saveTask = async (req,res) => {
 
+    if(!req.body.name || !req.body.description) return res.status(400).send("Incomplete data");
 
 }
 
