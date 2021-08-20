8a4c39f39929cfb672c90a83177e096fdd82b95d
Anthony Ahumada <locopala99@gmail.com>
2021-08-20 08:16:29 +0200

    more js

--- Mean/Project/ScrumBoard/backend/routes/user.js
+++ Mean/Project/ScrumBoard/backend/routes/user.js
@@ -8,12 +8,30 @@ const Auth = require("../middleware/auth");
 
 const validateUser = require("../middleware/ValidateUser");
 
-const admin = require("../middleware/admin");
+const Admin = require("../middleware/admin");
+const { relativeTimeRounding } = require("moment");
 
 router.post("/registerUser", Usercontroller.registerUser);
 
 //http://localhost:3001/api/user/listUser/Peppa
-router.get("/listUser/:name?", Auth, validateUser, Usercontroller.listUser);
+router.get(
+  "/listUser/:name?",
+  Auth,
+  validateUser,
+  Admin,
+  Usercontroller.listUser
+);
 
+router.post(
+  "/registerAdmin",
+  Auth,
+  validateUser,
+  Admin,
+  Usercontroller.registerAdmin
+);
+
+router.put("/updateUser", Auth, validateUser, Admin, Usercontroller.updateUser);
+
+router.put("/removeUser", Auth, validateUser, Admin, Usercontroller.deleteUser);
 
 module.exports = router;
